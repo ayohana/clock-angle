adc3925488f7ead18ea819a58993c0d7f1df767f
Adela <adela.yohana@gmail.com>
2020-02-24 09:54:33 -0800

    Initial Commit

---
+++ clock-angle.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Enter a time:");
+    string inputTime = Console.ReadLine();
+    Console.WriteLine(inputTime);
+  }
+}
