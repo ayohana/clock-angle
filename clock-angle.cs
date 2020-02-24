b72b9dfac97f30d4c28f4bbfc4de51a1e7cbd80d
Adela <adela.yohana@gmail.com>
2020-02-24 10:23:38 -0800

    Add a constructor

--- clock-angle.cs
+++ clock-angle.cs
@@ -1,12 +1,33 @@
 using System;
 using System.Collections.Generic;
 
+public class Clock
+{
+  // Constructor =====================
+  public string InputTime;
+  public string[] TimeArr;
+  public int HourHand;
+  public int MinHand;
+
+  // Constructor Method ===============
+  public Clock(string InputTime)
+  {
+    InputTime = InputTime;
+    TimeArr = InputTime.Split(":");
+    HourHand = int.Parse(TimeArr[0]);
+    MinHand = int.Parse(TimeArr[1]);
+  }
+}
+
 public class Program
 {
   public static void Main()
   {
     Console.WriteLine("Enter a time:");
-    string inputTime = Console.ReadLine();
-    Console.WriteLine(inputTime);
+    string input = Console.ReadLine();
+    Clock inputClock = new Clock(input);
+    Console.WriteLine(inputClock.HourHand);
+
   }
+
 }
