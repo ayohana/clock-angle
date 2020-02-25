3412a3561285c9104c8687fb6450c2c4819248e5
Adela <adela.yohana@gmail.com>
2020-02-24 16:02:43 -0800

    Add method to calculate smaller distance between clock hands

--- clock-angle.cs
+++ clock-angle.cs
@@ -17,19 +17,55 @@ public class Clock
     TimeArr = inputTime.Split(":");
     HourHand = Convert.ToDouble(TimeArr[0]);
     MinHand = Convert.ToDouble(TimeArr[1]);
-    StartAngle();
-    EndAngle();
+    GetHourAngle();
+    GetMinAngle();
   }
 
-  public void StartAngle() // does not require return value
-  // public int StartAngle() // requires return value
+  public void GetHourAngle() // does not require return value
+  // public double GetHourAngle() // requires return value
   {
+    if (HourHand >= 12)
+    {
+      HourHand = HourHand - 12;
+    }
     HourAngle = 0.5 * (60 * HourHand + MinHand);
   }
 
-  public void EndAngle()
+  public void GetMinAngle()
   {
     MinAngle = 6 * MinHand;
+  }
+
+  public bool BiggerHourAngle()
+  {
+    return (HourAngle >= MinAngle);
+  }
+
+  public double BiggerThan180(double result)
+  {
+    if (result >= 180)
+    {
+      return 360 - result;
+    }
+    else
+    {
+      return result;
+    }
+  }
+
+  public double CalculateAngle()
+  {
+    double result;
+    if (BiggerHourAngle())
+    {
+      result = HourAngle - MinAngle;
+      return BiggerThan180(result);
+    }
+    else
+    {
+      result = MinAngle - HourAngle;
+      return BiggerThan180(result);
+    }
   }
 
 }
@@ -41,10 +77,7 @@ public class Program
     Console.WriteLine("Enter a time:");
     string input = Console.ReadLine();
     Clock inputClock = new Clock(input);
-    Console.WriteLine(inputClock.HourHand);
-    Console.WriteLine(inputClock.HourAngle);
-    Console.WriteLine(inputClock.MinHand);
-    Console.WriteLine(inputClock.MinAngle);
+    Console.WriteLine("The distance between the hour and minute hands on an analog clock is " + inputClock.CalculateAngle());
   }
 
 }
